f96cd671345f9e1ac7c5c78e84c0aa3893dbd30f
codelimitless8 <codelimitless8@gmail.com>
2024-03-07 08:42:44 +0530

    first commit

---
+++ src/test/java/com/thorabh/samplejenkins/SampleJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thorabh.samplejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
